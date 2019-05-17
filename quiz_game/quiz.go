6933ff6dfa45d3ed1f32e3be629cea9e1e056da0
Jordan <jordantmarshall3@gmail.com>
2019-05-17 11:19:13 -0500

    add 404check upload csv script

--- quiz_game/quiz.go
+++ quiz_game/quiz.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 // Question struct to hold a question and answer
@@ -20,6 +21,8 @@ type Question struct {
 func main() {
 	// Read filename from command line flag, otherwise default to "problems.csv"
 	csvFlag := flag.String("csv", "problems.csv", "CSV filename for questions (default is \"problems.csv\"")
+	// Quiz timer flag
+	timerFlag := flag.Int("timer", 30, "Set time limit of quiz (default is 30sec)")
 	flag.Parse()
 	// open questions file, and read csv
 	csvFile, _ := os.Open(*csvFlag)
@@ -47,6 +50,22 @@ func main() {
 
 	// Store user's score
 	score := 0
+
+	// Ask user to hit Enter to start the Quiz
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Hit ENTER to Start")
+	_, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Start timer
+	timer := time.NewTimer(time.Duration(*timerFlag) * time.Second)
+	go func() {
+		<-timer.C
+		fmt.Printf("\nTimes up!\nYou scored %d out of %d\n", score, len(questions))
+		os.Exit(0)
+	}()
 
 	// Quiz loop through list of questions asking them to user
 	for i, quest := range questions {
